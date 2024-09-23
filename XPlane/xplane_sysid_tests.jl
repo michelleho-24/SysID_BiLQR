b7d538c034e8eb33e8a170850dffe85fa6f96e7c
Michelle Ho <mtho@SOE-ED99CF1D.stanford.edu>
2024-09-22 19:56:12 -0700

    update

--- XPlane/xplane_sysid_tests.jl
+++ XPlane/xplane_sysid_tests.jl
@@ -9,7 +9,7 @@ include("../Baselines/MPC.jl")
 include("../Baselines/random_policy.jl")
 # include("../Baselines/Regression.jl")
 
-function xplane_sysid(seed, pomdp, b0, Σ0, iters = 50)
+function xplane_sysid(seed, pomdp, Σ0, iters = 50)
     Random.seed!(seed)
     # pomdp = XPlanePOMDP()
 
